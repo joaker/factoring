22997f8d1bbca5d8eb44274e85494e1c3e0e9af8
John Oakley <jooakley@gmail.com>
2023-04-30 12:47:48 -0500

    create borrower model

---
+++ db/migrate/20230430174634_create_borrowers.rb
@@ -0,0 +1,9 @@
+class CreateBorrowers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :borrowers do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_30_174112) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_30_174634) do
+  create_table "borrowers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "invoices", force: :cascade do |t|
     t.string "invoice_number", null: false
     t.decimal "amount"
