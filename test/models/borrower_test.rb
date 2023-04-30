3be69f52c471dc210f4e60bf1cdce443c50b35a7
John Oakley <jooakley@gmail.com>
2023-04-30 13:16:00 -0500

    Add borrower test

--- test/models/borrower_test.rb
+++ test/models/borrower_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class BorrowerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid borrower' do
+    borrower = Borrower.new(name: 'ACME Bank')
+    assert borrower.valid?
+  end
+
+  test 'invalid without name' do
+    borrower = Borrower.new()
+    assert_not borrower.valid?, 'borrower is not valid without a name'
+    assert_not_nil borrower.errors[:name], 'no validation errors for name present'
+  end
 end
